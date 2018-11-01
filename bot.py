54aa4fe28836ee4f6fc441e1403efabcfda7d951
Anton Shcherbakov <anton@scherbakov.top>
2018-11-01 22:39:40 +0300

    Overall bot is v0.91 (proxy + some formatting)

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import logging
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton, ParseMode, ChatAction
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, CallbackQueryHandler
+
+from functools import wraps
 
 from utils import validate_request, build_menu
 from network import make_request_for_search, make_request_for_person
@@ -9,8 +11,37 @@ from network import make_request_for_search, make_request_for_person
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-updater = Updater(token='606272084:AAEyMyKaioythxTGakMpUvO4YmnHUgbc0UU')
+TOKEN = '606272084:AAEyMyKaioythxTGakMpUvO4YmnHUgbc0UU'
+REQUEST_KWARGS = {
+    'proxy_url': 'socks5://holdtherope.ru:1080',
+    # # Optional, if you need authentication:
+    'urllib3_proxy_kwargs': {
+        'username': 'proxy-user',
+        'password': 'xo2Qa6Pgxhq6Fu6',
+    }
+}
+
+updater = Updater(TOKEN, request_kwargs=REQUEST_KWARGS)
 dispatcher = updater.dispatcher
+
+
+# https://github.com/python-telegram-bot/python-telegram-bot/wiki/Code-snippets#send-a-chat-action
+def send_typing_action(func):
+    """Sends typing action while processing func command."""
+
+    @wraps(func)
+    def command_func(*args, **kwargs):
+        bot, update = args
+        chat_id = None
+        try:
+            chat_id = update.message.chat_id
+        except:
+            chat_id = update.callback_query.message.chat.id
+        bot.send_chat_action(chat_id=chat_id,
+                             action=ChatAction.TYPING)
+        func(bot, update, **kwargs)
+
+    return command_func
 
 
 def start(bot, update):
@@ -25,6 +56,7 @@ start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 
+@send_typing_action
 def text(bot, update):
     """
     Function for handling text-messages
@@ -39,6 +71,7 @@ def text(bot, update):
         elif amount == 1:
             bot.send_message(chat_id=update.message.chat_id,
                              text=result,
+                             parse_mode=ParseMode.MARKDOWN,
                              reply_markup=remove_special_keyboard)
         else:
             button_list = []
@@ -60,6 +93,7 @@ text_handler = MessageHandler(Filters.text, text)
 dispatcher.add_handler(text_handler)
 
 
+@send_typing_action
 def callback(bot, update):
     remove_special_keyboard = ReplyKeyboardRemove()
     person_id = int(update.callback_query.data)
@@ -68,6 +102,7 @@ def callback(bot, update):
 
     bot.send_message(chat_id=update.callback_query.message.chat.id,
                      text=result,
+                     parse_mode=ParseMode.MARKDOWN,
                      reply_markup=remove_special_keyboard)
 
 
--- utils.py
+++ utils.py
@@ -49,12 +49,66 @@ def get_office_position(person):
     return None
 
 
+def get_templated_string(field_name, obj, template):
+    if field_name == 'Транспортные средства':
+        return template % (obj['type']['name'])
+    elif field_name == 'Недвижимое имущество':
+        return template % (obj['type']['name'], obj['square'], obj.get('own_type', {}).get('name', ''))
+    elif field_name == 'Доход':
+        return template % (obj['size'], obj['comment'])
+    return
+
+
+def create_part_of_answer(field_name, data, template):
+    result = "\n*%s*\n" % field_name
+    for obj in data:
+        if obj['relative'] is None:
+            result += "%s\n" % get_templated_string(
+                field_name, obj, template)
+        else:
+            result += "%s: %s\n" % (obj['relative']['name'],
+                                    get_templated_string(field_name, obj, template))
+    return result
+
+
 def parse_person_answer(data):
     if isinstance(data, bytes):
         data = data.decode('utf8').replace("'", '"')
         data = json.loads(data)
 
-    return data['count']
+    all_years = data['results']
+    result = ""
+    if all_years:
+        last_year = all_years[-1]
+        result += "%s\n%s\n" % (last_year['main']['office']
+                                ['post'], last_year['main']['office']['name'])
+
+        incomes = last_year['incomes']
+        if incomes:
+            # maybe no comment
+            result += create_part_of_answer("Доход", incomes, "%s руб. (%s)")
+
+        real_estates = last_year['real_estates']
+        if real_estates:
+            result += create_part_of_answer("Недвижимое имущество",
+                                            real_estates, "%s, %s кв. м. (%s)")
+
+        vehicles = last_year['vehicles']
+        if vehicles:    # need more information
+            result += create_part_of_answer(
+                "Транспортные средства", vehicles, "%s")
+
+        savings = last_year['savings']
+        if savings:
+            result += "\n*Счета*\n"
+            for saving in savings:
+                result += "%s\n" % saving
+
+        # spendings
+        # stocks
+
+    # split result
+    return result[:1000]
 
 
 def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
